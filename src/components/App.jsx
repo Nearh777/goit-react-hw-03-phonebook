8080491a20b5b96d51c343fa434eb42be45908ac
Nearh777 <nearh777@gmail.com>
2023-05-23 15:58:22 +0300

    finish localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,22 +6,16 @@ import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { ContainerApp, Title, TitleCont } from './App.styled';
 
-
-
 export class App extends Component {
   static propTypes = {
     contacts: PropTypes.arrayOf(PropTypes.string),
     filter: PropTypes.string,
-
   };
-  
 
   state = {
     contacts: [],
     filter: '',
   };
-
-  
 
   addContact = (name, number) => {
     const { contacts } = this.state;
@@ -37,56 +31,63 @@ export class App extends Component {
 
   deleteContact = contactId => {
     this.setState(prevState => ({
-contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }))
-  }
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
 
-
-  changeFilter = (event) => {
-    this.setState({filter: event.target.value});
-  }
+  changeFilter = event => {
+    this.setState({ filter: event.target.value });
+  };
 
   getVisibleContact = () => {
-    const {filter, contacts} = this.state;
+    const { filter, contacts } = this.state;
     const normalizedFilter = filter.toLowerCase();
 
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    
+    if (parsedContacts){
+      this.setState({contacts: parsedContacts});
+    }
+    
   }
-  
 
-
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
-    const {filter, contacts} = this.state;
-    const {number} = this.props;
-    
+    const { filter, contacts } = this.state;
+    const { number } = this.props;
 
     const filteredContacts = this.getVisibleContact();
-    
-    
+
     return (
       <ContainerApp>
         <Title>Phonebook</Title>
 
-        <ContactForm contacts={contacts} addContact={this.addContact} number={number} />
+        <ContactForm
+          contacts={contacts}
+          addContact={this.addContact}
+          number={number}
+        />
 
         <TitleCont>Contacts:</TitleCont>
-       <Filter value={filter} onChange={this.changeFilter}/>
-       
-        <ContactList contacts={filteredContacts } onDeleteContatct={this.deleteContact}  />
-        
-        
-        
+        <Filter value={filter} onChange={this.changeFilter} />
+
+        <ContactList
+          contacts={filteredContacts}
+          onDeleteContatct={this.deleteContact}
+        />
       </ContainerApp>
     );
   }
 }
-
-
-
-
-
-
-
